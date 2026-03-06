bc844d32900d29bc5bf5497280a2a6c89ecb2dda
yash5123 <yashsrai5123@gmail.com>
2026-03-07 00:49:37 +0530

    fix

--- frontend/src/pages/FindHospitals.jsx
+++ frontend/src/pages/FindHospitals.jsx
@@ -5,6 +5,9 @@ import {
   fetchHospitalsFromOSM,
   calculateDistance,
 } from "../utils/geoUtils";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 import {
   MapContainer,
@@ -48,17 +51,12 @@ export default function FindHospitals() {
 
 
   useEffect(() => {
-    if (!L || !L.Icon || !L.Icon.Default) return;
-
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl:
-        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-      iconUrl:
-        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-      shadowUrl:
-        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+      iconRetinaUrl: markerIcon2x,
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
     });
   }, []);
 
